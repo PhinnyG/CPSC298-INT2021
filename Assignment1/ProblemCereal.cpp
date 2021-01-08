9c1b0cf7c91236850f1ee3ceaa067ca76061bda6
PhinnyG <fwhite@chapman.edu>
2021-01-08 03:06:41 +0000

    Added FireCap problem

--- Assignment1/ProblemCereal.cpp
+++ Assignment1/ProblemCereal.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 
 using namespace std;
+
 const double METRIC_TON_IN_OZ = 35273.92;
 double cerealWeightOz;
 double cerealWeightTons;
@@ -11,44 +12,44 @@ void userInput();
 void output();
 
 int main() {
-   userInput();
-   cerealWeightTons = ouncesToTons(cerealWeightOz);
-   output();
+    userInput();
+    cerealWeightTons = ouncesToTons(cerealWeightOz);
+    output();
 
-   return 0;
+    return 0;
 }
 
 //method to convert ounces to metric tons
 double ouncesToTons(double ounces) {
-   return ounces / METRIC_TON_IN_OZ;
+    return ounces / METRIC_TON_IN_OZ;
 }
 
 //method to determine how many times a weight in ounces goes into a metric ton
 double numToMakeTon(double ounces) {
-   return METRIC_TON_IN_OZ / ounces;
+    return METRIC_TON_IN_OZ / ounces;
 }
 
 //handles the user interaction
 void userInput() {
-   cout << endl << "Enter the weight of your cereal in ounces: ";
-   cin >> cerealWeightOz;
-   cout << endl;
+    cout << endl << "Enter the weight of your cereal in ounces: ";
+    cin >> cerealWeightOz;
+    cout << endl;
 
-   //ensures an input greater than 0
-   while(cerealWeightOz <= 0) {
-      cout << "Please enter a value greater than 0: ";
-      cin >> cerealWeightOz;
-   }
+    //ensures an input greater than 0
+    while(cerealWeightOz <= 0) {
+        cout << "Please enter a value greater than 0: ";
+        cin >> cerealWeightOz;
+    }
 
-   cout << endl;
+    cout << endl;
 }
 
 //formats and prints the output to console
 void output() {
-   cout << "The cereal weighs " << cerealWeightTons << " metric tons." << endl;
-   cout << "You would need " << ceil(numToMakeTon(cerealWeightOz)) << " (" << numToMakeTon(cerealWeightOz);
-   cout << " to be exact) boxes to have one metric ton of cereal!" << endl;
-   cout << endl;
+    cout << "The cereal weighs " << cerealWeightTons << " metric tons." << endl;
+    cout << "You would need " << ceil(numToMakeTon(cerealWeightOz)) << " (" << numToMakeTon(cerealWeightOz);
+    cout << " to be exact) boxes to have one metric ton of cereal!" << endl;
+    cout << endl;
 }
 
 
---
+++ Assignment1/ProblemFireCapacity.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int maxRoomCap;
+int attendees;
+int distanceFromCap;
+bool isOverFull(int roomCap, int numPeople);
+void input();
+void output(bool atCap, int distanceFromCap);
+void posCheck(int *num);
+
+int main() {
+
+    input();
+    bool fullCheck = isOverFull(maxRoomCap, attendees);
+    output(fullCheck, distanceFromCap);
+
+    return 0;
+}
+
+//checks to see if the room is full or not and computes the distance from the capacity
+bool isOverFull(int roomCap, int numPeople) {
+    if (roomCap >= numPeople) {
+        distanceFromCap = roomCap - numPeople;
+        return false;
+    }
+    else {
+        distanceFromCap = numPeople - roomCap;
+        return true;
+    }
+}
+
+//handles the user input
+void input() {
+    cout << endl;
+    cout << "Please enter the maximum room capacity: ";
+    cin >> maxRoomCap;
+    posCheck(&maxRoomCap);
+    cout << "Please enter the number of people attending: ";
+    cin >> attendees;
+    posCheck(&attendees);
+    cout << endl;
+}
+
+//handles output formatting to console
+void output(bool atCap, int distanceFromCap) {
+    string plurality;
+    if (distanceFromCap == 1)
+        plurality = "person";
+    else
+        plurality = "people";
+
+    if (atCap) {
+        cout << "Slow down there Mr. Popular. Please exclude your " << distanceFromCap;
+        cout << " least favorite " << plurality << " to meet fire regulations." << endl;
+    }
+    else if (distanceFromCap != 0) {
+        cout << "Not a very popular meeting? You can invite " << distanceFromCap << " more " << plurality  << " and still be within fire regulations." << endl;
+    }
+    else {
+        cout << "Its a full house! Congratulations on filling up the place without violating fire regulations." << endl;
+    }
+
+    cout << endl;
+}
+
+//ensures user input is only positive
+void posCheck(int *num) {
+    while (*num <= 0) {
+        cout << "Please enter a value greater than 0: ";
+        cin >> *num;
+    }
+}
+
+
